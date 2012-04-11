1bfb46b6597f1d2b1045641b5c27ca892a6292d4
Yuri Buyanov <yuri.buyanov@gmail.com>
2012-04-11 22:52:02 +0400

    test for SET register, [memory address]

--- dcpu-tests/DCOpCodesTest.m
+++ dcpu-tests/DCOpCodesTest.m
@@ -25,7 +25,7 @@ DCEmulator* _emulator;
     _emulator = nil;
 }
 
-- (void)testSet {
+- (void)testSetRegLiteral {
 
     //test literal
     int b = 0x3f; //literal 0x1f
@@ -35,22 +35,49 @@ DCEmulator* _emulator;
     GHTestLog($str(@"Instruction for \"SET X, 0x1f\": %04x", instr));
     [_emulator exec:instr];
     GHAssertEquals([_emulator getValue:X], (UInt16)0x1f, @"SET X, 0x1f");
-    
-    //test memory
+    GHAssertEquals(_emulator->cycles, 1l, @"SET REG, LITERAL should take 1 cycle");
+}
+
+- (void)testSetMemReg {
+    //test memory write
     //SET [0x1000], X
+    UInt16 val  = 0x1234;
+    UInt16 addr = 0x1000;
+    [_emulator setValue:val for:X]; //set x
+
     UInt16 program[2] = {
-            SET | (NWP << 4) | (X << 10), //
-            0x1000
+        SET | (NWP << 4) | (X << 10), //
+        addr
     };
-    GHAssertEquals([_emulator getValue:PC], (UInt16)0, @"PC check");
     GHTestLog($str(@"Instruction for \"SET [0x1000], X\": %04x %04x", program[0], program[1]));
     [_emulator loadBinary:&program[0] withLength:2];
     //TODO: rewrite for actual program execution
     [_emulator setValue:0x0001 for:PC]; //emulate running program
     [_emulator exec:program[0]];
     GHAssertEquals([_emulator getValue:PC], (UInt16)2, @"PC should be incremented after setting to memory pointer");
-    GHAssertEquals(_emulator->mem[0x1000], (UInt16)0x1f, @"Value 0x1f should be copied from X to memory");
+    GHAssertEquals(_emulator->mem[addr], val, @"Value 0x1f should be copied from X to memory");
+    GHAssertEquals(_emulator->cycles, 1l, @"SET [ADDR], REG should take 1 cycle");
+}
 
+- (void)testSetRegMem {
+    //test memory read
+    //SET Y, [0x1000]
+    UInt16 val  = 0x1234;
+    UInt16 addr = 0x1000;
+
+    UInt16 program[2] = {
+        SET | (Y << 4) | (NWP << 10), //
+        addr
+    };
+    _emulator->mem[addr] = val; //put test value into memory
+    GHTestLog($str(@"Instruction for \"SET Y, [0x1000]\": %04x %04x", program[0], program[1]));
+    [_emulator loadBinary:&program[0] withLength:2];
+    //TODO: rewrite for actual program execution
+    [_emulator setValue:0x0001 for:PC]; //emulate running program
+    [_emulator exec:program[0]];
+    GHAssertEquals([_emulator getValue:PC], (UInt16)2, @"PC should be incremented after setting to memory pointer");
+    GHAssertEquals([_emulator getValue:Y], val, @"Value 0x1f should be copied from memory to Y");
+    GHAssertEquals(_emulator->cycles, 2l, @"SET REG, [ADDR] should take 2 cycles");
 }
 
 
