16eb1be97c68dae74f6feb095d4e0d0ba0fa46fa
Yuri Buyanov <yuri.buyanov@gmail.com>
2012-04-11 22:30:53 +0400

    test for SET [memory address], register

--- dcpu-tests/DCOpCodesTest.m
+++ dcpu-tests/DCOpCodesTest.m
@@ -26,14 +26,31 @@ DCEmulator* _emulator;
 }
 
 - (void)testSet {
+
+    //test literal
     int b = 0x3f; //literal 0x1f
     int a = X;
-    int op = SET;
     
-    UInt16 instr = op | (a << 4) | (b << 10);
+    UInt16 instr = SET | (a << 4) | (b << 10); //0xfc31
     GHTestLog($str(@"Instruction for \"SET X, 0x1f\": %04x", instr));
     [_emulator exec:instr];
     GHAssertEquals([_emulator getValue:X], (UInt16)0x1f, @"SET X, 0x1f");
+    
+    //test memory
+    //SET [0x1000], X
+    UInt16 program[2] = {
+            SET | (NWP << 4) | (X << 10), //
+            0x1000
+    };
+    GHAssertEquals([_emulator getValue:PC], (UInt16)0, @"PC check");
+    GHTestLog($str(@"Instruction for \"SET [0x1000], X\": %04x %04x", program[0], program[1]));
+    [_emulator loadBinary:&program[0] withLength:2];
+    //TODO: rewrite for actual program execution
+    [_emulator setValue:0x0001 for:PC]; //emulate running program
+    [_emulator exec:program[0]];
+    GHAssertEquals([_emulator getValue:PC], (UInt16)2, @"PC should be incremented after setting to memory pointer");
+    GHAssertEquals(_emulator->mem[0x1000], (UInt16)0x1f, @"Value 0x1f should be copied from X to memory");
+
 }
 
 
